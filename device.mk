e0de530ca8fce9d7e2cb4e1aa2a8caadee28a5a1
Jeff Sharkey <jsharkey@android.com>
2013-03-13 17:08:18 -0700

    Use dumpstate directly for bugreports.
    Use new broadcast flag on dumpstate to notify framework when a
    bugreport finished.  Remove older shell scripts.
    
    Bug: 7005318
    Change-Id: Ic639961b087c2cad8c71dcf68bc68bddf7068af6

--- device.mk
+++ device.mk
@@ -238,13 +238,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
         debug.egl.recordable.rgba8888=1
 
-# for bugmailer
-PRODUCT_PACKAGES += send_bug
-PRODUCT_COPY_FILES += \
-	system/extras/bugmailer/bugmailer.sh:system/bin/bugmailer.sh \
-	system/extras/bugmailer/send_bug:system/bin/send_bug
-
-
 # NFC packages
 PRODUCT_PACKAGES += \
     libnfc-nci \
