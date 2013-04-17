56d9d67a1b8f71036814a7e4c07294495de08fb5
andy2_kuo <andy2_kuo@asus.com>
2013-04-17 15:50:19 -0700

    wlan: creat init.flo.wifi.sh prevent wifi turn off fail caused by timing issue.
    Change-Id: I062da827f7d91efe3d46ad6a0bed7e53bb4ef55f
    Signed-off-by: andy2_kuo <andy2_kuo@asus.com>
    Signed-off-by: Ed Tam <etam@google.com>

--- device.mk
+++ device.mk
@@ -51,7 +51,8 @@ PRODUCT_COPY_FILES += \
 	device/asus/flo/WCNSS_cfg.dat:system/vendor/firmware/wlan/prima/WCNSS_cfg.dat \
 	device/asus/flo/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
 	device/asus/flo/WCNSS_qcom_wlan_nv_flo.bin:system/etc/wifi/WCNSS_qcom_wlan_nv_flo.bin \
-	device/asus/flo/WCNSS_qcom_wlan_nv_deb.bin:system/etc/wifi/WCNSS_qcom_wlan_nv_deb.bin
+	device/asus/flo/WCNSS_qcom_wlan_nv_deb.bin:system/etc/wifi/WCNSS_qcom_wlan_nv_deb.bin \
+	device/asus/flo/init.flo.wifi.sh:system/etc/init.flo.wifi.sh
 
 PRODUCT_COPY_FILES += \
 	device/asus/flo/snd_soc_msm_2x_Fusion3:system/etc/snd_soc_msm/snd_soc_msm_2x_Fusion3 \
