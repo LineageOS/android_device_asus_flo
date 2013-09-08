e69871a62550d60252bd0b64e94e29c4f8ad60b2
Steve Kondik <shade@chemlab.org>
2013-09-08 11:48:57 -0700

    flo: Use cyanogen_flo_defconfig
    Change-Id: Ief2adde655d448a1e452c19ae0dff03473ede0ff

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -49,7 +49,7 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 
 # Try to build the kernel
 TARGET_KERNEL_SOURCE := kernel/google/msm
-TARGET_KERNEL_CONFIG := flo_defconfig
+TARGET_KERNEL_CONFIG := cyanogen_flo_defconfig
 
 BOARD_USES_ALSA_AUDIO:= true
 BOARD_USES_LEGACY_ALSA_AUDIO:= false
