43bfcb022c2c0532f33d37a8625333bc55a1468a
Kelvie Wong <kelvie@ieee.org>
2013-09-15 08:19:27 -0700

    flo: Refactor the things in init.flo.rc into init.flo.common.rc
    This is so that this can be included in "deb".
    
    Change-Id: Ia255560568b7951a98c5d353db085dd962dc6234

--- device-common.mk
+++ device-common.mk
@@ -72,6 +72,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
 	device/asus/flo/init.flo.usb.rc:root/init.flo.usb.rc \
+	device/asus/flo/init.flo.common.rc:root/init.flo.common.rc \
 	device/asus/flo/ueventd.flo.rc:root/ueventd.flo.rc \
 	device/asus/flo/media_profiles.xml:system/etc/media_profiles.xml \
 	device/asus/flo/media_codecs.xml:system/etc/media_codecs.xml
