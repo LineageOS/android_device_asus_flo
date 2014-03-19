80253018a4950dabf04b9cd7d44bf56852021082
Lajos Molnar <lajos@google.com>
2014-03-19 14:19:56 -0700

    stagefright: use media_codecs_google*.xml files
    Bug: 13549114
    Change-Id: Id1a548f6338f00035dcd41c9f8a681129fb9fd1d

--- device-common.mk
+++ device-common.mk
@@ -75,6 +75,8 @@ PRODUCT_COPY_FILES += \
 	device/asus/flo/init.flo.usb.rc:root/init.flo.usb.rc \
 	device/asus/flo/ueventd.flo.rc:root/ueventd.flo.rc \
 	device/asus/flo/media_profiles.xml:system/etc/media_profiles.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
 	device/asus/flo/media_codecs.xml:system/etc/media_codecs.xml
 
 PRODUCT_COPY_FILES += \
