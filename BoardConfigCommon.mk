033cc01fad8f2204d64d02b266bc1a9bb8b6bd03
Steve Kondik <shade@chemlab.org>
2014-03-27 22:03:20 -0700

    flo: Use the correct Power HAL
    Change-Id: I64ad22c0b7cfc98b14afb245550d86d66dcda530

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -65,9 +65,6 @@ TARGET_BOARD_PLATFORM := msm8960
 
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/asus/flo/bluetooth
 
-# Use the CM PowerHAL
-TARGET_USES_CM_POWERHAL := true
-
 # FIXME: HOSTAPD-derived wifi driver
 BOARD_HAS_QCOM_WLAN := true
 BOARD_WLAN_DEVICE := qcwcn
