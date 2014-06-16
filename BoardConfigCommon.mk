dba9973dd7c03c2a18770c78945461ea91311d63
Victoria Lease <violets@google.com>
2014-06-16 14:42:09 -0700

    enable EXTENDED_FONT_FOOTPRINT for flo/deb
    please enjoy your new selection of CJK fonts!
    
    Bug: 15569561
    Change-Id: Ia9cacbe788e0ebcd4f34e44fc6edad4f621f47f7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -129,3 +129,9 @@ BOARD_SEPOLICY_UNION += \
         te_macros \
         thermald.te \
         ueventd.te
+
+# Enable Minikin text layout engine (will be the default soon)
+USE_MINIKIN := true
+
+# Include an expanded selection of fonts
+EXTENDED_FONT_FOOTPRINT := true
