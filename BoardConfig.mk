ae57c9a0893afd6289682725ca696049fdc35438
Christopher Ferris <cferris@google.com>
2014-07-10 12:26:54 -0700

    Indicate this uses dlmalloc.
    (cherry picked from commit 342bc9a1b2422488b67acb1043331dbf17e00d1d)
    
    Change-Id: Ic271b8861f411fca53ce2a54bbce58f9993baa0b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,5 +24,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := device/asus/flo
 
 TARGET_RECOVERY_FSTAB = device/asus/flo/fstab.flo
 
+MALLOC_IMPL := dlmalloc
+
 -include vendor/asus/flo/BoardConfigVendor.mk
 include device/asus/flo/BoardConfigCommon.mk
