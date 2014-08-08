bb567e81a48c082b8961d1e773e28a3f5102cb4f
Eino-Ville Talvala <etalvala@google.com>
2014-08-08 15:08:45 -0700

    Switch to the correct camera feature list
    Flo does not have a flash.
    
    Bug: 16900409
    Change-Id: I48e3c5d7b48208e88159fb5e12180a02b90799c0

--- device-common.mk
+++ device-common.mk
@@ -104,7 +104,7 @@ PRODUCT_COPY_FILES += \
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+	frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
 	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
 	frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
 	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
