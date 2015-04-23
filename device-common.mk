51b56d99b3fb5e923e72b8c06a466ed7f306eed9
Adam Lesinski <adamlesinski@google.com>
2015-04-23 09:51:50 +0300

    deb/flo: Remove density from PRODUCT_AAPT_CONFIG
    AAPT ignores densities in PRODUCT_AAPT_CONFIG.
    The use of PRODUCT_AAPT_PREF_CONFIG for density is
    encouraged, as AAPT is able to determine the fallback
    density to use if a resource of the specified density
    does not exist.
    
    Change-Id: I581fe61e1f2afcf0f0b490ba49ebee55f47053c6

--- device-common.mk
+++ device-common.mk
@@ -35,7 +35,7 @@ endif
 # currently contain all of the bitmaps at xhdpi density so
 # we do this little trick to fall back to the hdpi version
 # if the xhdpi doesn't exist.
-PRODUCT_AAPT_CONFIG := normal large hdpi xhdpi
+PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 PRODUCT_PACKAGES := \
