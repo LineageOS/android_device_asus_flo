3336b8e64b5cb3925312a115c0afcdd66219e825
Dan Albert <danalbert@google.com>
2015-05-30 14:55:05 -0700

    Remove stlport from flo.
    Bug: http://b/15193147
    Change-Id: I5c99212d7f1467f3885ff5bf9e2698def8cdf94a

--- device-common.mk
+++ device-common.mk
@@ -55,10 +55,6 @@ PRODUCT_PACKAGES += \
         VisualizationWallpapers \
         librs_jni
 
-# http://b/15193147
-# TODO(danalbert): Remove this once stlport is dead and gone.
-PRODUCT_PACKAGES +=  libstlport
-
 PRODUCT_COPY_FILES += \
 	device/asus/flo/WCNSS_cfg.dat:system/vendor/firmware/wlan/prima/WCNSS_cfg.dat \
 	device/asus/flo/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
