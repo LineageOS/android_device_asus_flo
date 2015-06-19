bafdfa59cc16a3b6766240fca43fc9bea48b59f8
Steve Kondik <steve@cyngn.com>
2015-06-19 20:10:48 +0300

    deb/flo: don't need to copy FFMPEG config anymore
    Change-Id: Iffc59d5ce30333788dce79d6b9a5d85a21918616

--- device-common.mk
+++ device-common.mk
@@ -77,7 +77,6 @@ PRODUCT_COPY_FILES += \
 	device/asus/flo/media_profiles.xml:system/etc/media_profiles.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-	frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
 	device/asus/flo/media_codecs.xml:system/etc/media_codecs.xml
 
 PRODUCT_COPY_FILES += \
