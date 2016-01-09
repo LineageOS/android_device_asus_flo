d13c691bbc5b4d511066dba38d3e7d14616fba4c
doc HD <doc.divxm@gmail.com>
2016-01-10 00:03:20 +0200

    flo: flo: Update fingerprint to MMB29O
    Change-Id: I1524875af8480008ada12c0a4ec79d84c7eec6df

--- cm.mk
+++ cm.mk
@@ -22,6 +22,6 @@ PRODUCT_MANUFACTURER := asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=razor \
-    BUILD_FINGERPRINT=google/razor/flo:6.0.1/MMB29K/2419427:user/release-keys \
-    PRIVATE_BUILD_DESC="razor-user 6.0.1 MMB29K 2419427 release-keys"
+    BUILD_FINGERPRINT=google/razor/flo:6.0.1/MMB29O/2459718:user/release-keys \
+    PRIVATE_BUILD_DESC="razor-user 6.0.1 MMB29O 2459718 release-keys"
 
