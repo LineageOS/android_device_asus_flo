294b35c86f2d597a34c17b6082f49eb9aa66f5d5
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-01-28 19:07:10 +0800

    flo: Boost smarter
    Reduce the unnecessary boosting by boosting CPU frequencies
    only when really necessary. Accomplish this by:
    
     * Boost only to 1.1 GHz for the duration of an ondemand
       sample whenever a touch input is registered.
    
     * Boost to 1 GHz for 3 seconds for CPU_BOOST and LAUNCH_BOOST
       hints, both of which are important UX moments.
    
    Change-Id: I06c2e7d662418a6cc1530e2887dd11d87a6e6309

--- power/power_flo.c
+++ power/power_flo.c
@@ -282,7 +282,8 @@ static void power_hint( __attribute__((unused)) struct power_module *module,
         return;
 
     switch (hint) {
-        case POWER_HINT_INTERACTION:
+        case POWER_HINT_LAUNCH_BOOST:
+        case POWER_HINT_CPU_BOOST:
             ALOGV("POWER_HINT_INTERACTION");
             touch_boost();
             break;
