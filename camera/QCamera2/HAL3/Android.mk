77670887ea7e4e62f1a94e5fb92fb5e3261a8a7f
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-01-31 14:23:46 +0800

    flo: Build camera HAL with kernel headers
    After removing the prebuilt kernel headers, we need to include
    the kernel headers from source to fix the build.
    
    Change-Id: I0e7074076db50032862ae4d8f001580405e9cd99

--- camera/QCamera2/HAL3/Android.mk
+++ camera/QCamera2/HAL3/Android.mk
@@ -35,6 +35,10 @@ LOCAL_C_INCLUDES += \
         $(call project-path-for,qcom-display)/libgralloc
 endif
 
+LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/media
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+
 LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libui libcamera_metadata
 
