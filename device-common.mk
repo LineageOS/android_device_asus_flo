807c93122d19fc4284d4a65ab89709c9d3a51591
Kim <elektroschmock78@googlemail.com>
2016-02-14 14:53:54 +0100

    flo/deb: remove wifi.supplicant_scan_interval
    It is an overlay now. No need to set the default value.
    
    https://github.com/CyanogenMod/android_frameworks_base/blob/cm-13.0/core/res/res/values/config.xml#L432
    
    Change-Id: Ic4a83cf4b1bfc56d2a5b564bdc942e1411ea428c

--- device-common.mk
+++ device-common.mk
@@ -247,8 +247,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	drm.service.enabled=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
-	wifi.interface=wlan0 \
-	wifi.supplicant_scan_interval=15
+	wifi.interface=wlan0
 
 # Enable AAC 5.1 output
 PRODUCT_PROPERTY_OVERRIDES += \
