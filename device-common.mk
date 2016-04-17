61d6fd31f9209bddaedd98366798c137226bdf79
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-04-17 10:42:59 +0800

    flo: Don't override locales
    This is already defined through the dependency chain.
    
    Change-Id: I0b595edd08e0fd4c3e3b4197997fdc55c00121ac

--- device-common.mk
+++ device-common.mk
@@ -285,7 +285,5 @@ PRODUCT_PACKAGES +=  libstlport
 PRODUCT_PACKAGES += \
     Snap
 
-PRODUCT_LOCALES := en_US es_US de_DE zh_CN
-
 $(call inherit-product, frameworks/native/build/tablet-7in-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, hardware/qcom/msm8960/msm8960.mk)
