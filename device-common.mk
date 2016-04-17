3dcc72808092b53dc654dc7164934f2f2649ef4f
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-04-17 10:42:59 +0800

    flo: Don't override locales
    A more diverse range of locales is already defined in the build.
    
    Change-Id: I0b595edd08e0fd4c3e3b4197997fdc55c00121ac

--- device-common.mk
+++ device-common.mk
@@ -298,7 +298,5 @@ PRODUCT_COPY_FILES += \
     device/asus/flo/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
     device/asus/flo/nfc/libnfc-brcm-20791b05.conf:system/etc/libnfc-brcm-20791b05.conf
 
-PRODUCT_LOCALES := en_US es_US de_DE zh_CN
-
 $(call inherit-product, frameworks/native/build/tablet-7in-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, hardware/qcom/msm8960/msm8960.mk)
