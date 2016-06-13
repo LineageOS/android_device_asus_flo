ba9d1ca1a321e8c659ba04b53e5d5db113ad1b5c
Suman Mukherjee <sumam@codeaurora.org>
2016-06-13 16:23:08 +0530

    BACKPORT: QCamera2: HAL1: Close duplicated FD's for media extension.
    In video metadata mode, framework allocates native handle
    resource and needs to be freed in release recording.
    This change is to free framework resources in case of media
    extension use cases.
    
    [ashwin]: Adapt for msm8x74 HAL
    
    Change-Id: I1bac10740130d506d71ec1275dd7d75f66c263c8

--- camera/QCamera2/HAL/QCamera2HWI.cpp
+++ camera/QCamera2/HAL/QCamera2HWI.cpp
@@ -483,6 +483,7 @@ int QCamera2HardwareInterface::recording_enabled(struct camera_device *device)
 void QCamera2HardwareInterface::release_recording_frame(
             struct camera_device *device, const void *opaque)
 {
+    int32_t ret = NO_ERROR;
     QCamera2HardwareInterface *hw =
         reinterpret_cast<QCamera2HardwareInterface *>(device->priv);
     if (!hw) {
@@ -492,7 +493,15 @@ void QCamera2HardwareInterface::release_recording_frame(
     ALOGD("%s: E", __func__);
 
     //Close and delete duplicated native handle and FD's
-    QCameraVideoMemory::closeNativeHandle(opaque, hw->mStoreMetaDataInFrame > 0);
+    if ((hw->mVideoMem != NULL)&&(hw->mStoreMetaDataInFrame>0)) {
+        ret = hw->mVideoMem->closeNativeHandle(opaque,TRUE);
+        if (ret != NO_ERROR) {
+            ALOGE("Invalid video metadata");
+            return;
+        }
+    } else {
+        ALOGW("Possible FD leak. Release recording called after stop");
+    }
 
     hw->lockAPI();
     int32_t ret = hw->processAPI(QCAMERA_SM_EVT_RELEASE_RECORIDNG_FRAME, (void *)opaque);
@@ -937,7 +946,8 @@ QCamera2HardwareInterface::QCamera2HardwareInterface(int cameraId)
       m_bStartZSLSnapshotCalled(false),
       m_pPowerModule(NULL),
       mDumpFrmCnt(0),
-      mDumpSkipCnt(0)
+      mDumpSkipCnt(0),
+	  mVideoMem(NULL)
 {
     mCameraDevice.common.tag = HARDWARE_DEVICE_TAG;
     mCameraDevice.common.version = HARDWARE_DEVICE_API_VERSION(1, 0);
@@ -1403,7 +1413,10 @@ QCameraMemory *QCamera2HardwareInterface::allocateStreamBuf(cam_stream_type_t st
                 bCachedMem = QCAMERA_ION_USE_NOCACHE;
             }
             ALOGD("%s: vidoe buf using cached memory = %d", __func__, bCachedMem);
-            mem = new QCameraVideoMemory(mGetMemory, bCachedMem);
+            QCameraVideoMemory *videoMemory = new QCameraVideoMemory(mGetMemory, bCachedMem);
+            mem = videoMemory;
+            mVideoMem = videoMemory;
+
         }
         break;
     case CAM_STREAM_TYPE_DEFAULT:
@@ -1698,6 +1711,7 @@ int QCamera2HardwareInterface::startRecording()
 {
     int32_t rc = NO_ERROR;
     ALOGD("%s: E", __func__);
+    mVideoMem = NULL;
     if (mParameters.getRecordingHintValue() == false) {
         ALOGE("%s: start recording when hint is false, stop preview first", __func__);
         stopChannel(QCAMERA_CH_TYPE_PREVIEW);
@@ -1724,6 +1738,7 @@ int QCamera2HardwareInterface::startRecording()
         }
     }
 #endif
+    mVideoMem = NULL;
     ALOGD("%s: X", __func__);
     return rc;
 }
