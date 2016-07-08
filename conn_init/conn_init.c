191fd98bf585bd89960ef393689f26b2fd1229ce
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-07-08 20:51:06 +0800

    flo: Match conn_init fread check with AOSP
    We were deviating a bit from the actual fread check in AOSP.
    Bring us somewhat in line with AOSP.
    
    This consists of parts of the patches:
    
    Conninit: Check fread return value
    
    Bug: 18632512
    
    (cherry picked from commit 832524f54092092b7f2031f97609f3ff698de08c)
    
    Change-Id: I47c851e98b769d0e78e9743de1bd9af0420462a4
    
    flo: conn_init: fix load macaddr
    
    fread returns number of blocks not byte.
    So cannot read mac address correctly in previous code.
    
    Change-Id: Idbbcb1bbbda8af30ec4554cd4bbec90515547759
    Signed-off-by: Young-Ho Cha <ganadist@gmail.com>

--- conn_init/conn_init.c
+++ conn_init/conn_init.c
@@ -29,7 +29,6 @@ static int wifi_check_qcom_cfg_files()
 
     // Read MAC String
     FILE *fp = NULL;
-    int n = 0;
     fp = fopen("/persist/wifi/.macaddr", "r");
     if ( fp == NULL )
     {
@@ -38,11 +37,13 @@ static int wifi_check_qcom_cfg_files()
     }
     else
     {
-        n = fread(macAddress, 12, 1, fp);
+        int n = fread(macAddress, 12, 1, fp);
         fclose(fp);
-        if (n == 0) {
+        if (n != 1) {
             // Buffer may be partially written. Reset.
             memset(macAddress, 0, sizeof(macAddress));
+            wfc_util_qcom_check_config((unsigned char *)macAddress);
+            return 0;
         }
 
         // Write MAC String
