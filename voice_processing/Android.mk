25e0bb9365aea2ef4cfa23bf19a0d4fb588742f6
Simon Shields <keepcalm444@gmail.com>
2016-11-15 08:55:25 +1100

    flo: cleanup the tree
    AOSP has abandoned us now, we may as well make things look a bit
    prettier.
    
    Change-Id: I09f48ae1b565f05b0ac0ae6bb18a4a2100402e45

--- voice_processing/Android.mk
+++ voice_processing/Android.mk
@@ -5,10 +5,10 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-	voice_processing_descriptors.c
+    voice_processing_descriptors.c
 
 LOCAL_C_INCLUDES += \
-	$(call include-path-for, audio-effects)
+    $(call include-path-for, audio-effects)
 
 LOCAL_MODULE := libqcomvoiceprocessingdescriptors
 
