14e36bfb6e36984492835853638edb5a3c6e52d0
Myles Watson <mylesgw@google.com>
2017-02-02 11:33:19 -0800

    Build the new Bluetooth HAL
    Test: Treehugger succeeds.
    Change-Id: Ibeb69b0764ee6ca3244d4026d69b80fd8ec368b9

--- device-common.mk
+++ device-common.mk
@@ -46,6 +46,10 @@ PRODUCT_PACKAGES += \
     hci_qcomm_init \
     init.flo.bt.sh
 
+# Bluetooth HAL
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1200
