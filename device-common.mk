102445f30f7c4d327fb0bfc4e1f3942f856696ae
Janis Danisevskis <jdanis@google.com>
2017-03-14 16:26:00 -0700

    flo: manifest: Make keymaster binderized always
    Bug: 35764921
    Test: Boot the device and check the output of lshal. The keymaster
          service must be running and should have two clients: vold and
          keystore
    
    Change-Id: Ia876034975bd64a9913351ca93b4642d91c30583

--- device-common.mk
+++ device-common.mk
@@ -123,7 +123,8 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 
 # Keystore
 PRODUCT_PACKAGES += \
