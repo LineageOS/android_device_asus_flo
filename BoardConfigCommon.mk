a8894e4b4373365958baf4172d661c587fc2daa4
Yifan Hong <elsk@google.com>
2017-05-09 15:36:42 -0700

    Add device compatibility matrix
    Test: m system_manifest.xml -j (checks compatibility against
        framework manifest)
    Bug: 37954458
    
    Change-Id: I45e4c5e8fc1824652be552842d3283e8f8cf7331

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -29,7 +29,7 @@ BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_IMAGE_NAME = zImage
 BOARD_KERNEL_PAGESIZE := 2048
 # BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=msm8960 maxcpus=2
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=flo user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=flo user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += vmalloc=340M
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 TARGET_KERNEL_SOURCE := kernel/google/msm
@@ -128,5 +128,6 @@ TARGET_RECOVERY_DENSITY := hdpi
 
 # Device manifest
 DEVICE_MANIFEST_FILE := device/asus/flo/manifest.xml
+DEVICE_MATRIX_FILE := device/asus/flo/compatibility_matrix.xml
 
 -include vendor/asus/flo/BoardConfigVendor.mk
