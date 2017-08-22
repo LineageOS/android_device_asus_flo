7314de3aba46916bd714c9744835975ad2a36e99
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-22 14:01:07 -0700

    flo: Move Lineage specific overlays to overlay-lineage
    Change-Id: Ic38bb1acb4d37f90b62679e4420815f49b30fc7f

--- device.mk
+++ device.mk
@@ -24,4 +24,7 @@ PRODUCT_PACKAGES += \
 # the actual meat of the device-specific product definition
 $(call inherit-product, device/asus/flo/device-common.mk)
 
-DEVICE_PACKAGE_OVERLAYS := device/asus/flo/overlay
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    device/asus/flo/overlay \
+    device/asus/flo/overlay-lineage
