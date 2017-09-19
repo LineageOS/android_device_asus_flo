d8d84699a81966d0f4240fc0e19eae1103433f8c
Artem Borisov <dedsa2002@gmail.com>
2017-09-19 21:04:47 +0300

    flo: build NFC HIDL service
    Change-Id: I00d912c382bdd7ca4eb4c23a9ec72ea8babc4de9

--- device-common.mk
+++ device-common.mk
@@ -138,6 +138,7 @@ PRODUCT_CHARACTERISTICS := tablet,nosdcard
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0-impl \
+    android.hardware.nfc@1.0-service \
     nfc_nci.bcm2079x.default \
     libnfc-nci \
     libnfc_nci_jni \
