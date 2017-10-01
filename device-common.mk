4053ea04b0dd1b332f0e161c32833d838f6779b5
Artem Borisov <dedsa2002@gmail.com>
2017-10-01 12:53:19 +0300

    flo: build bcm nfc implementation
    Change-Id: Iac7f369e637e21a8ea582ccdb6b3071b1d2915b4

--- device-common.mk
+++ device-common.mk
@@ -141,7 +141,7 @@ PRODUCT_CHARACTERISTICS := tablet,nosdcard
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0-impl \
+    android.hardware.nfc@1.0-impl-bcm \
     android.hardware.nfc@1.0-service \
     nfc_nci.bcm2079x.default \
     libnfc-nci \
