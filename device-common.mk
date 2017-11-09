1ed1cbc8b3eca1c7fd0562e221c929bd2c381f45
Artem Borisov <dedsa2002@gmail.com>
2017-11-09 17:45:37 +0300

    flo: enable binderized gralloc service
    There is no reason to keep using passthrough, as our binder works properly now.
    
    Change-Id: I9b72e9e703cfccc1c9a21f39264412cda0a6e233

--- device-common.mk
+++ device-common.mk
@@ -74,6 +74,7 @@ PRODUCT_PACKAGES += \
 # Graphics
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
