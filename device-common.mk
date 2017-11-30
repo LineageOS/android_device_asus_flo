4ee774f5e6c13c7ab838a69e4a9d0ab2ce68defc
Artem Borisov <dedsa2002@gmail.com>
2017-11-30 15:49:21 +0300

    flo: always build Recorder app
    Even though Recorder is not included in mini config, it is not dramatically big and should fit in our /system.
    
    Change-Id: I8b463bc36804e40d9cd6400635e808bfe1980bb0

--- device-common.mk
+++ device-common.mk
@@ -202,6 +202,10 @@ PRODUCT_PACKAGES += \
     ueventd.rc \
     ueventd.flo.rc
 
+# Recorder
+PRODUCT_PACKAGES += \
+    Recorder
+
 # RenderScript HAL
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
