9566a274444b1ff5caa19c2e1c42b5e9a0f97259
Artem Borisov <dedsa2002@gmail.com>
2017-12-02 15:46:24 +0300

    flo: move device-specific modules to vendor
    Change-Id: I79e26fe58dd393b9da04f3c99b3eadd263c40afa

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -54,23 +54,3 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES := etc/ueventd.flo.rc
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
-
-# Bluetooth/Wifi config scripts
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := init.flo.bt.sh
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES := etc/init.flo.bt.sh
-LOCAL_MODULE_PATH := $(TARGET_OUT)/bin
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := init.flo.wifi.sh
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES := etc/init.flo.wifi.sh
-LOCAL_MODULE_PATH := $(TARGET_OUT)/bin
-include $(BUILD_PREBUILT)
-
-
--- rootdir/etc/init.flo.wifi.sh
+++ rootdir/etc/init.flo.wifi.sh
@@ -4,7 +4,7 @@
 rm /data/misc/wifi/WCNSS_qcom_cfg.ini
 rm /data/misc/wifi/WCNSS_qcom_wlan_nv.bin
 
-/system/bin/conn_init
+/vendor/bin/conn_init
 
 echo 1 > /dev/wcnss_wlan
 
