11b177eabccfff80a04c0a5c0e113532e24cd0ee
Artem Borisov <dedsa2002@gmail.com>
2018-01-02 12:18:47 +0300

    Revert "flo: build NFC HIDL service"
    This reverts commit d8d84699a81966d0f4240fc0e19eae1103433f8c.
    
    Change-Id: Ibc3b8356387f022ba8fb2201622cd39488916275

--- device-common.mk
+++ device-common.mk
@@ -149,7 +149,6 @@ PRODUCT_CHARACTERISTICS := tablet,nosdcard
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0-impl-bcm \
-    android.hardware.nfc@1.0-service \
     nfc_nci.bcm2079x.default \
     libnfc-nci \
     libnfc_nci_jni \
