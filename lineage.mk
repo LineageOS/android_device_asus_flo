a5ebe533d8ce130eae23e33a1fd0a170431ff6b0
David Marble <drmarble1@gmail.com>
2018-04-03 13:04:54 -0500

    Flo: Fix vendor image mismatch
    Needed after https://review.lineageos.org/#/c/208868/
    
    Change-Id: Ie3fa5ae4891e2e61b6d31c1e44e7ecbbf4841f1a

--- lineage.mk
+++ lineage.mk
@@ -30,5 +30,6 @@ PRODUCT_MANUFACTURER := asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=razor \
-    BUILD_FINGERPRINT=google/razor/flo:6.0.1/MOB30X/3036618:user/release-keys \
     PRIVATE_BUILD_DESC="razor-user 6.0.1 MOB30X 3036618 release-keys"
+
+BUILD_FINGERPRINT := google/razor/flo:6.0.1/MOB30X/3036618:user/release-keys
