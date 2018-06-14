b063b47387bd8b53f15bb02ac0c08228e4217210
Artem Borisov <dedsa2002@gmail.com>
2018-06-14 17:59:21 +0300

    Revert "flo: Build Snap camera"
    Snap doesn't go very well with our new camera hal.
    Let's use glorious Camera2 instead.
    
    This reverts commit 5547916083919ad0e5dfd045c40cc112cf9d7a7b.
    
    Change-Id: I799b852b4f51bd8929121701fd6fc32bd89bc717

--- device-common.mk
+++ device-common.mk
@@ -69,8 +69,7 @@ TARGET_BOOTANIMATION_HALF_RES := true
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    camera.device@3.2-impl \
-    Snap
+    camera.device@3.2-impl
 
 # Charger
 PRODUCT_PACKAGES += \
