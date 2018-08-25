47a7104d59277988aa03208dab131a6c0e826163
dianlujitao <dianlujitao@lineageos.org>
2018-08-25 19:07:05 +0800

    flo: Rename bt props for new HAL
    Change-Id: I1afd110bffed0f43b1afcbef99be8a3776d24e0b

--- rootdir/etc/init.flo.bt.sh
+++ rootdir/etc/init.flo.bt.sh
@@ -39,7 +39,7 @@ else
 BDADDR=`/vendor/bin/bdAddrLoader -p net.btdut.address -s -x`
 fi
 
-setprop bluetooth.status off
+setprop vendor.bluetooth.status off
 
 logi "BDADDR: $BDADDR"
 
@@ -65,11 +65,11 @@ fi
 case $? in
   0) logi "Bluetooth QSoC firmware download succeeded, $PWR_CLASS $BDADDR $TRANSPORT";;
   *) failed "Bluetooth QSoC firmware download failed" $exit_code_hci_qcomm_init;
-     setprop bluetooth.status off;
+     setprop vendor.bluetooth.status off;
      exit $exit_code_hci_qcomm_init;;
 esac
 
-setprop bluetooth.status on
+setprop vendor.bluetooth.status on
 
 logi "start bluetooth smd transport"
 
