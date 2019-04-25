5463ed13077f4399873dc7fe014e50dc3cc47ad6
padarshr <padarshr@codeaurora.org>
2019-04-25 17:58:17 +0530

    flo: Address 'LOCAL_MODULE_TAGS := eng debug' deprecation.
    Newer AOSP deprecates LOCAL_MODULE_TAGS := eng debug.
    We now remove that and instead add the package to
    PRODUCT_PACKAGES_ENG or PRODUCT_PACKAGES_DEBUG as applicable.
    
    To ensure that we don't miss out on any package during this switch,
    we add these explicitly, even though it might have been already
    added under PRODUCT_PACKAGES at some other location.
    
    Change-Id: I35bd67c3e7752be5bcd8f8682864062173e92724

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -6,7 +6,7 @@ ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),flo)
 # deb also has fstab.flo and init.flo.rc
 include $(CLEAR_VARS)
 LOCAL_MODULE := fstab.flo
-LOCAL_MODULE_TAGS := optional eng
+LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES := etc/fstab.flo
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
@@ -14,7 +14,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := init.flo.rc
-LOCAL_MODULE_TAGS := optional eng
+LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES := etc/init.flo.rc
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
@@ -24,7 +24,7 @@ endif
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := init.flo.power.rc
-LOCAL_MODULE_TAGS := optional eng
+LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES := etc/init.flo.power.rc
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
@@ -32,7 +32,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := init.recovery.flo.rc
-LOCAL_MODULE_TAGS := optional eng
+LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES := etc/init.flo.power.rc
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
@@ -41,7 +41,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := init.flo.usb.rc
-LOCAL_MODULE_TAGS := optional eng
+LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES := etc/init.flo.usb.rc
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
@@ -49,7 +49,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := ueventd.flo.rc
-LOCAL_MODULE_TAGS := optional eng
+LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES := etc/ueventd.flo.rc
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
