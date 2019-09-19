a46c2289e7e1ccff612f6aa2d3da88d355c140c5
David Marble <drmarble1@gmail.com>
2019-09-19 04:21:22 +0200

    Revert "flo: Include device/qcom/sepolicy-legacy"
    This reverts commit 6416f0d38a9f298847372f98f5fc2dd65e2dc6ba.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Change-Id: I863137e8accf76823e8729a86a11b2ceba1cbc5e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -126,8 +126,6 @@ USE_DEVICE_SPECIFIC_CAMERA:= true
 
 HAVE_ADRENO_SOURCE:= false
 
-# SELinux
-include device/qcom/sepolicy-legacy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += device/asus/flo/sepolicy
 
 # Media HAL
