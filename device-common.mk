c32ce4870610936b7bb9a485f12777f058a037e2
Bruno Martins <bgcngm@gmail.com>
2019-10-04 23:20:42 +0100

    flo: Switch to legacy Wi-Fi service
    Change-Id: I690bc80349e47907a29a89b8a6f96659f1c61aba

--- device-common.mk
+++ device-common.mk
@@ -242,7 +242,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi@1.0-service.legacy \
     conn_init \
     dhcpcd.conf \
     wificond \
