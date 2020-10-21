09c97a93f9e38bf576494391e4404296c3c406e8
Arne Coucheron <arco68@gmail.com>
2020-10-22 00:18:07 +0300

    flo: gps: Cleanup, update config and add sap.conf
    Change-Id: I5d3052bea15a2a3a686256275ae85bd61cca5d36

--- gps/core/Android.mk
+++ gps/core/Android.mk
@@ -27,8 +27,8 @@ LOCAL_SRC_FILES += \
 LOCAL_CFLAGS += \
      -fno-short-enums \
      -D_ANDROID_ \
-     -Wno-unused-parameter \
      -Wno-error \
+     -Wno-unused-parameter
 
 LOCAL_C_INCLUDES:= \
     $(TARGET_OUT_HEADERS)/gps.utils
