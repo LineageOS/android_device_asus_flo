780a8a0b2d0a1614916ab1eb65e59a7a5e3da3ca
Arne Coucheron <arco68@gmail.com>
2020-10-22 00:18:07 +0300

    flo: gps: Cleanup, update config and add sap.conf
    Change-Id: I5d3052bea15a2a3a686256275ae85bd61cca5d36

--- gps/etc/Android.mk
+++ gps/etc/Android.mk
@@ -1,14 +1,17 @@
-
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(filter flo deb,$(TARGET_DEVICE)),)
 include $(CLEAR_VARS)
-
 LOCAL_MODULE := gps.conf
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/
 LOCAL_SRC_FILES := gps.conf
-
 include $(BUILD_PREBUILT)
-endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := sap.conf
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/
+LOCAL_SRC_FILES := sap.conf
+include $(BUILD_PREBUILT)
